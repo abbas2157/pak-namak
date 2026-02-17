5bebae0e934eef1eb7827f0c8e9658b969b448c3
Mudassar Abbas <abbas8156@gmail.com>
2026-02-17 22:03:09 +0500

    Rotes setup

--- routes/admin.php
+++ routes/admin.php
@@ -8,20 +8,17 @@ Route::middleware('web')->group(function () {
     });
 
     Route::middleware('auth')->group(function () {
-        Route::group(['prefix' => 'admin'], function(){
+        Route::get('logout', [App\Http\Controllers\Admin\AuthController::class, 'logout'])->name('logout');
+        Route::get('/', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
 
-            Route::get('logout', [App\Http\Controllers\Admin\AuthController::class, 'logout'])->name('logout');
-            Route::get('/', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
-
-            Route::resource('purchases', App\Http\Controllers\Admin\PurchaseController::class, ['as' => 'admin']);
-            Route::resource('productions', App\Http\Controllers\Admin\ProductionController::class, ['as' => 'admin']);
-            Route::resource('vendors', App\Http\Controllers\Admin\VendorController::class, ['as' => 'admin']);
-            Route::resource('sales', App\Http\Controllers\Admin\SaleController::class, ['as' => 'admin']);
-            Route::resource('shops', App\Http\Controllers\Admin\ShopController::class, ['as' => 'admin']);
-            Route::resource('types', App\Http\Controllers\Admin\TypeController::class, ['as' => 'admin']);
-            Route::resource('employees', App\Http\Controllers\Admin\EmployeeController::class, ['as' => 'admin']);
-            Route::resource('expenses', App\Http\Controllers\Admin\ExpenseController::class, ['as' => 'admin']);
-            Route::resource('assets', App\Http\Controllers\Admin\AssetController::class);
-        });
+        Route::resource('purchases', App\Http\Controllers\Admin\PurchaseController::class, ['as' => 'admin']);
+        Route::resource('productions', App\Http\Controllers\Admin\ProductionController::class, ['as' => 'admin']);
+        Route::resource('vendors', App\Http\Controllers\Admin\VendorController::class, ['as' => 'admin']);
+        Route::resource('sales', App\Http\Controllers\Admin\SaleController::class, ['as' => 'admin']);
+        Route::resource('shops', App\Http\Controllers\Admin\ShopController::class, ['as' => 'admin']);
+        Route::resource('types', App\Http\Controllers\Admin\TypeController::class, ['as' => 'admin']);
+        Route::resource('employees', App\Http\Controllers\Admin\EmployeeController::class, ['as' => 'admin']);
+        Route::resource('expenses', App\Http\Controllers\Admin\ExpenseController::class, ['as' => 'admin']);
+        Route::resource('assets', App\Http\Controllers\Admin\AssetController::class);
     });
 });
